5ef417b9f1e2debde90e37e07224899c38c02fde
David <david.v.bernardo@gmail.com>
2022-09-20 11:45:57 -0500

    commit tree 932ef42303f0c35cfa064e82136ea56c68ee58bd

---
+++ index.ts
@@ -0,0 +1,13 @@
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    var bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    var bestCustomer = 'maybe bob';
+}
